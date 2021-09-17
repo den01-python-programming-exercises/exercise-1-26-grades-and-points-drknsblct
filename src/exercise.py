5a82c5d10dd210a379c3d9ffa4bf696032ed74a5
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:35:00 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -2,22 +2,22 @@ def main():
     #write your code below this line
     num = int(input('Give points [0-100]:'))
     
-if num < 0:
-    print('impossible')
-elif num <= 49:
-    print('failed')
-elif num <= 59:
-    print('1')
-elif num <= 69:
-    print('2')
-elif num <= 79:
-    print('3')
-elif num <= 89:
-    print('4')
-elif num <= 100:
-    print('5')
-else:
-    print('incredible!')
+    if num < 0:
+        print('impossible')
+    elif num <= 49:
+        print('failed')
+    elif num <= 59:
+        print('1')
+    elif num <= 69:
+        print('2')
+    elif num <= 79:
+        print('3')
+    elif num <= 89:
+        print('4')
+    elif num <= 100:
+        print('5')
+    else:
+        print('incredible!')
 
 if __name__ == '__main__':
     main()
